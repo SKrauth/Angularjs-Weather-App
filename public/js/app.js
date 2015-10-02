0c7ed362f492cf454adbbbbe0482d452f7567b90
SKrauth <spkrauth@gmail.com>
2015-10-02 18:33:41 -0400

    working api call with display

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,26 @@
 var app = angular.module("hipsterWeather", ["ngRoute"]);
 
-app.controller("HomeController", function(){
+app.controller("HomeController", function($scope){
 
 });
 
-app.controller("WeatherController", function($scope){
-
+app.controller("WeatherController", function($scope, $http){
+    $scope.latitude = 0;
+    $scope.longitude = 0;
+    $scope.weatherData = {};
+    var baseUrl = "https://api.forecast.io/forecast/";
+    var baseUrlwApi = baseUrl + apiKey + "/";
+    
+    $scope.getWeather = function(){
+        $http.jsonp(baseUrlwApi + $scope.latitude + "," + $scope.longitude + "?callback=JSON_CALLBACK")
+        .success(function(response){
+            $scope.weatherData = response;
+            console.log($scope.weatherData);
+        })
+        .error(function(err){
+            console.log(err);
+        });
+    };
 });
 
 app.config(function($routeProvider){
